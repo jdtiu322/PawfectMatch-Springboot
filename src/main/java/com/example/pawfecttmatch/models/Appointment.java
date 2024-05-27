14602a187dc9d27a6e168cdff8b66841522d18ba
jdtiu322 <jdtiu322@gmail.com>
2024-05-28 05:09:59 +0800

    fixed bugs

--- src/main/java/com/example/pawfecttmatch/models/Appointment.java
+++ src/main/java/com/example/pawfecttmatch/models/Appointment.java
@@ -2,6 +2,11 @@ package com.example.pawfecttmatch.models;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Appointment {
     private String appointmentId;
     private String matchId;
@@ -11,59 +16,4 @@ public class Appointment {
     private String location;
     private Date createdAt;
 
-    public String getAppointmentId() {
-        return appointmentId;
-    }
-
-    public void setAppointmentId(String appointmentId) {
-        this.appointmentId = appointmentId;
-    }
-
-    public String getMatchId() {
-        return matchId;
-    }
-
-    public void setMatchId(String matchId) {
-        this.matchId = matchId;
-    }
-
-    public String getUser1Id() {
-        return user1Id;
-    }
-
-    public void setUser1Id(String user1Id) {
-        this.user1Id = user1Id;
-    }
-
-    public String getUser2Id() {
-        return user2Id;
-    }
-
-    public void setUser2Id(String user2Id) {
-        this.user2Id = user2Id;
-    }
-
-    public Date getScheduledDate() {
-        return scheduledDate;
-    }
-
-    public void setScheduledDate(Date scheduledDate) {
-        this.scheduledDate = scheduledDate;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
 }
--- src/main/java/com/example/pawfecttmatch/models/Match.java
+++ src/main/java/com/example/pawfecttmatch/models/Match.java
@@ -2,41 +2,16 @@ package com.example.pawfecttmatch.models;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Match {
     private String matchId;
     private String user1Id;
     private String user2Id;
     private Date createdAt;
 
-    public String getMatchId() {
-        return matchId;
-    }
-
-    public void setMatchId(String matchId) {
-        this.matchId = matchId;
-    }
-
-    public String getUser1Id() {
-        return user1Id;
-    }
-
-    public void setUser1Id(String user1Id) {
-        this.user1Id = user1Id;
-    }
-
-    public String getUser2Id() {
-        return user2Id;
-    }
-
-    public void setUser2Id(String user2Id) {
-        this.user2Id = user2Id;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+   
 }
